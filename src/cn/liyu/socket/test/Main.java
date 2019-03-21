057c964a22fce73ed62bfbab10f43b2e05ee7f93
1046242635 <1046242635@qq.com>
2019-03-21 14:55:25 +0800

    Initial commit

---
+++ src/cn/liyu/socket/test/Main.java
@@ -0,0 +1,8 @@
+package cn.liyu.socket.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/cn/liyu/socket/test/TomcatServer.java
@@ -0,0 +1,15 @@
+package cn.liyu.socket.test;
+
+import java.util.Date;
+
+public class TomcatServer {
+
+    private final static int PORT=8080;
+
+    public static void main(String[] args) {
+        Date date = new Date();
+
+        SecurityManager securityManager = System.getSecurityManager();
+    }
+
+}
